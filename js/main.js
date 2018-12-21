1b214befe60b158537ac078e47524e6723dd067c
Everett Grassler <dragwar9@gmail.com>
2018-12-21 10:04:26 -0600

    Added semicolon + one small css change

--- js/main.js
+++ js/main.js
@@ -10,10 +10,10 @@ var markers = [];
 if ('serviceWorker' in navigator) {
   navigator.serviceWorker.register('./sw.js')
   .then((response) => {
-    console.log('%c SW Register... Success!', 'background-color: black; color: lightblue;', response)
+    console.log('%c SW Register... Success!', 'background-color: black; color: lightblue;', response);
   })
   .catch((error) => {
-    console.warn('%c SW Register... Failed.', 'background-color: black; color: lightblue;', error)
+    console.warn('%c SW Register... Failed.', 'background-color: black; color: lightblue;', error);
   });
 }
 
