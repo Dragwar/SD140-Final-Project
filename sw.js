d3b546d06afc686c3ce54bad7dac66d42eb41095
Everett Grassler <dragwar9@gmail.com>
2019-01-16 11:22:28 -0600

    Now caches all relevant maps and minor formatting fix
    Credit to Jerry for the list of urls for caching all the
    maps.

--- sw.js
+++ sw.js
@@ -1,3 +1,6 @@
+const __My_Map_Box_Key = 'access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw';
+
+
 // When sw gets installed then this will run
 // How to add things into a cache
 const staticCache = 'SD140FinalProject-cache-1';// IMPORTANT: increment the num when you change something in the cache
@@ -39,18 +42,126 @@ self.addEventListener('install', (e) => {
           './restaurant.html?id=8',
           './restaurant.html?id=9',
           './restaurant.html?id=10',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1208/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1203/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1208/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1203/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
-          'https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1539.jpg70?access_token=pk.eyJ1IjoiZXZlcmV0dGciLCJhIjoiY2pwd3lzdDU2MGs1MDQ4bHNldThrcnZ3YiJ9.aUrRJi0nahDPriMou3M6iw',
+
+          // Credit to Jerry for this tedious work 
+          // (caches all relevant maps for all restaurant pages)
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1207/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1205/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1206/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1204/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1203/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1203/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1208/1539.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/12/1208/1540.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24639.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24639.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19302/24646.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19302/24647.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19302/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19303/24646.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19303/24647.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19303/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24630.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24630.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19308/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19308/24642.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19308/24643.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19309/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19309/24642.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19309/24643.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19293/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19293/24649.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19293/24650.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19294/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19294/24649.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19294/24650.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19299/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19296/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19296/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19296/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19296/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19305/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19305/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19305/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19306/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19306/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19306/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24634.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19305/24634.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24639.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24639.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24646.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24647.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24646.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24647.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19304/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24630.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24633.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19297/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24636.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24649.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19307/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19307/24643.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19310/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19310/24642.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19310/24643.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19292/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19292/24650.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24648.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24649.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19295/24650.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19298/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24635.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24637.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24638.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24632.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24631.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19300/24630.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19308/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19309/24640.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19307/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19310/24641.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19307/24642.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19292/24649.jpg70?${__My_Map_Box_Key}`,
+          `https://api.tiles.mapbox.com/v4/mapbox.streets/16/19301/24636.jpg70?${__My_Map_Box_Key}`,
+
           './app-icons/favicon.ico',
           './app-icons/favicon-32x32.png',
           './app-icons/favicon-16x16.png',
@@ -60,7 +171,7 @@ self.addEventListener('install', (e) => {
         ]
       );
     })
-    .catch((err) => console.warn('make cache ERROR:', err))
+      .catch((err) => console.warn('make cache ERROR:', err))
   );
 
 });
@@ -75,12 +186,12 @@ self.addEventListener('activate', (e) => {
         cacheNames.filter((cacheName) => {
           return cacheName.startsWith('SD140FinalProject-') && cacheName !== staticCache;
         })
-        .map((cacheName) => {
-          return caches.delete(cacheName);
-        })
+          .map((cacheName) => {
+            return caches.delete(cacheName);
+          })
       );
     })
-    .catch((err) => console.warn('delete cache ERROR:', err))
+      .catch((err) => console.warn('delete cache ERROR:', err))
   );
 
 });
@@ -90,14 +201,14 @@ self.addEventListener('activate', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request)
-    .then((response) => {
-      console.log(response);
-      return response;
-    })
-    .then((response) => {
-      return response || fetch(e.request);
-    })
-    .catch((err) => console.warn('retrieve from cache ERROR:', err))
+      .then((response) => {
+        console.log(response);
+        return response;
+      })
+      .then((response) => {
+        return response || fetch(e.request);
+      })
+      .catch((err) => console.warn('retrieve from cache ERROR:', err))
   );
 
 });
